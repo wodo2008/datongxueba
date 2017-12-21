4ff32d8aeb76681075458e421399614efe22320d
liujun4719@163.com <liujun4719@163.com>
2017-12-21 21:24:01 +0800

    start

---
+++ grad_detail_backend/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class GradDetailBackendConfig(AppConfig):
+    name = 'grad_detail_backend'
